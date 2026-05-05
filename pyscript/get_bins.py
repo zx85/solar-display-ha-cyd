96e3613ac17847f8b7946173a7cbbb01cb20b065
james h <github@mus-ic.co.uk>
2026-05-05 23:21:42 +0100

    update the python scripts

---
+++ pyscript/get_bins.py
@@ -0,0 +1,60 @@
+# Bin lookup script
+# This uses the Waste Collection Schedule HACS integration:
+# https://ha.mus-ic.co.uk/hacs/repository/254347436
+# And once the Calendar is setup, it looks up the bins for tomorrow and creates
+# a sensor concatenating the first 2 letters of the bin colour (e.g. bk for
+# black, bu for blue, br for brown)
+
+import datetime
+
+# lower case and just the colour
+bin_lookup = {"black": "bk", "blue": "bu", "brown": "br"}
+
+
+@service
+# if it's "set" - look for tomorrow's bins (runs hourly between 3-9pm)
+# otherwise clear the bins (normally scheduled at 12pm)
+# (this is because the calendar doesn't show today's events if they're all-day)
+def get_bins(status=None):
+    if status != "set":
+        log.info("Clearing bins.")
+        state.set(
+            "sensor.upcoming_bins", value=0, bins="", target_date="", mode="clear"
+        )
+        return
+    # let's get tomorrow's bins
+    now = datetime.datetime.now()
+    target_date = now + datetime.timedelta(days=1)
+    target_str = target_date.strftime("%Y-%m-%d")
+    bin_list = ""
+    bin_qty = 0
+
+    # Fetch events from the calendar
+    # We fetch a 2-day window to ensure we catch the relevant events
+    result = calendar.get_events(
+        entity_id="calendar.west_suffolk_council",
+        start_date_time=now,
+        end_date_time=now + datetime.timedelta(days=2),
+    )
+
+    events = result.get("calendar.west_suffolk_council", {}).get("events")
+    log.info(f"Events is {events}")
+
+    # Filter for all-day events matching our target date
+    for event in events:
+        start = event.get("start", {})
+        # All-day events use the 'date' key instead of 'dateTime'
+        if summary := event.get("summary"):
+            bin_name = summary.lower().split(" ")[0]
+            if start == target_str:
+                bin_list = f'{bin_list}{bin_lookup.get(bin_name,"")}'
+                bin_qty += 1
+
+    # 4. Update a sensor so you can use this in your dashboard
+    state.set(
+        "sensor.upcoming_bins",
+        value=bin_qty,
+        bins=bin_list,
+        target_date=target_str,
+        mode="ready",
+    )
--- pyscript/solar_data.py
+++ pyscript/solar_data.py
@@ -1,29 +1,32 @@
 import json
 
+
 @service
 def get_solar_data():
-    states={}
-    state_list={"solar_in":"sensor.solis_ac_output_total_power",        # current solar power
-        "power_used": "sensor.solis_total_consumption_power",           # current consumption
-        "grid_in": "sensor.solis_power_grid_total_power",               # current grid power
-        "battery_per": "sensor.solis_remaining_battery_capacity",       # % battery remaining
-        "export_today":"sensor.solis_daily_on_grid_energy",             # exported today
-        "solar_today":"sensor.solis_energy_today",                      # solar today
-        "grid_in_today":"sensor.solis_daily_grid_energy_purchased",     # grid today
-        "cur_rate":"sensor.octopus_energy_electricity_18p0906942_1012934837063_current_rate", # current Octopus rate
-        "solis_charging":"input_boolean.solar_battery_charging",         # Solis charging
-        "solis_discharging":"input_boolean.solar_battery_discharging",  # Solis discharging
-        "power_up": "input_boolean.octopus_power_up_active",            # Octopus Power ups
-        "car_charging": "select.myenergi_zappi_2_charge_mode",          # Zappi charge mode
-        }
-    for state_label,state_name in state_list.items():
-        states[state_label]=state.get(state_name)
-    states['timestamp']=state.getattr(sensor.solis_total_consumption_power)['Last updated']
-    states['bins']=state.getattr(sensor.upcoming_bins)['bins']
-    presence_string=''
-    for person in ['james']:
-        if state.get(f'device_tracker.{person.lower()}_phone') == 'home':
-            presence_string+=(person[:1]).replace('L','LE') # Shim for name changes
-    states['presence']=presence_string
-    state.set("input_text.solar_display_data", value=states['timestamp'], info=states)
-    
+    states = {}
+    state_list = {
+        "solar_in": "sensor.solis_ac_output_total_power",  # current solar power
+        "power_used": "sensor.solis_total_consumption_power",  # current consumption
+        "grid_in": "sensor.solis_power_grid_total_power",  # current grid power
+        "battery_per": "sensor.solis_remaining_battery_capacity",  # % battery remaining
+        "export_today": "sensor.solis_daily_on_grid_energy",  # exported today
+        "solar_today": "sensor.solis_energy_today",  # solar today
+        "grid_in_today": "sensor.solis_daily_grid_energy_purchased",  # grid today
+        "cur_rate": "sensor.octopus_energy_electricity_current_rate",  # current Octopus rate
+        "solis_charging": "input_boolean.solar_battery_charging",  # Solis charging
+        "solis_discharging": "input_boolean.solar_battery_discharging",  # Solis discharging
+        "power_up": "input_boolean.octopus_power_up_active",  # Octopus Power ups
+        "car_charging": "select.myenergi_zappi_2_charge_mode",  # Zappi charge mode
+    }
+    for state_label, state_name in state_list.items():
+        states[state_label] = state.get(state_name)
+    states["timestamp"] = state.getattr(sensor.solis_total_consumption_power)[
+        "Last updated"
+    ]
+    states["bins"] = state.getattr(sensor.upcoming_bins)["bins"]
+    presence_string = ""
+    for person in ["james"]:
+        if state.get(f"device_tracker.{person.lower()}_phone") == "home":
+            presence_string += (person[:1]).replace("L", "LE")  # Shim for name changes
+    states["presence"] = presence_string
+    state.set("input_text.solar_display_data", value=states["timestamp"], info=states)
